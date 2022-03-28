cda1f9578cbad0f5d6cb5b233290c2af6857b5e4
tchescoM <tchesco@live.nl>
2022-03-27 21:58:00 -0300

    first commit

---
+++ pizzas.js
@@ -0,0 +1,11 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:8, name:'Lisboa', img:'images/pizza8.png', price:28.50, sizes:['330g', '540g', '900g'], description:'Descrição da pizza lisboa em mais de uma linha muito legal bem interessante'},
+    {id:9, name:'Catumilho', img:'images/pizza9.png', price:29.50, sizes:['340g', '550g', '910g'], description:'Descrição da pizza catumilho em mais de uma linha muito legal bem interessante'}
+];
